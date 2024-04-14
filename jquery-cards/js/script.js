17a28748da7aa8aced6d0e6b8284bc6c0d8ee8f7
kudosincorporated <40593930+kudosincorporated@users.noreply.github.com>
2024-04-14 22:56:36 +1000

    Add files via upload

--- jquery-cards/js/script.js
+++ jquery-cards/js/script.js
@@ -1,67 +1,68 @@
 
-
-
-
-const updateCards = (cards) => {
-
-    $('#hand').html('');
-    $('#tabs').html('');
-
-    let $lastPage = $('<div>').addClass('card').attr('id', 'lastPage');
-    let $lastPageIcon = $('<div>').addClass('card').attr('id', 'lastPageIcon');
-
-    for (let i = 0; i < cards.length; i++) {
-        const card = cards[i];
-
-        $('#hand').append( returnCardDOM(card) );
-        $('#tabs').append( returnCardDOM(card) );
-        
-        let $block = $('<div>').addClass('block');
-
-        let $name = $('<div>').addClass('name').text(card.name);
-        let $inverted = $('<div>').addClass('subtitle').text(card.inverted ? "Reversed" : "");
-        let $facing = $('<div>').text(card.facing);
-        let $reversed = $('<div>').text(card.reversed);
-        let $advice = $('<div>').text(card.advice);
-
-        $block.append($name);
-        $block.append($inverted);
-        if (!card.inverted) {
-            $block.append($facing);
-        } else {
-            $block.append($reversed);
-        }
-        $block.append($advice);
-
-        $lastPage.append($block);
-    }
-    $('#hand').append($lastPage);
-
-    $lastPageIcon.append('≡');
-    $('#tabs').append($lastPageIcon);
-
-}
-
 const returnCardDOM = (card) => {
-
     let $card = $('<div>').addClass('card');
     if (card.flipped) $card.addClass('flipped');
     if (card.inverted) $card.addClass('inverted');
 
-    let $image = $('<img>').attr('src', 'cards/' + card.img).addClass('image');
-    let $back = $('<img>').attr('src', 'cards/back.jpg').addClass('back');
+    let $inner = $('<div>').addClass('inner');
 
-    $card.append($image);
-    $card.append($back);
-    //$card.append(card.name);
+    let $front = $('<div>').addClass('front');
+    let $back = $('<div>').addClass('back');
+
+    let $frontImg = $('<img>').attr('src', 'cards/' + card.img);
+    let $backImg = $('<img>').attr('src', 'cards/back.jpg');
+
+    $front.append($frontImg);
+    $back.append($backImg);
+
+    $inner.append($front);
+    $inner.append($back);
+
+    $card.append($inner);
 
     return $card;
+}
 
+const returnInfoDOM = (card) => {
+    let $info = $('<div>').addClass('info');
+
+    let $name = $('<div>').addClass('name').text(card.name);
+    let $inverted = $('<div>').addClass('subtitle').text(card.inverted ? "Reversed" : "");
+    let $facing = $('<div>').text(card.facing);
+    let $reversed = $('<div>').text(card.reversed);
+    let $advice = $('<div>').text(card.advice);
+
+    $info.append($name);
+    $info.append($inverted);
+    if (!card.inverted) {
+        $info.append($facing);
+    } else {
+        $info.append($reversed);
+    }
+    $info.append($advice);
+
+    return $info;
+}
+
+const returnPageDOM = () => {
+    let $card = $('<div>').addClass('card lastPageEver');
+    let $inner = $('<div>').addClass('inner');
+    
+    let $icon = $('<div>').addClass('icon').text('?');
+    let $content = $('<div>').addClass('content desc');
+
+    $inner.append($icon);
+    $inner.append($content);
+
+    $card.append($inner);
+
+    return $card;
 }
 
 $(function() {
     $.getJSON("tarot-images.json", function(data) {
 
+        // Assign data
         tarotCards = data.cards;
         
         // Shuffle
@@ -70,41 +71,47 @@ $(function() {
         // Assign reversed
         tarotCards.forEach(card => card.inverted = Math.random() < 0.5);
 
-        // Assign flipped
-        tarotCards.forEach(card => card.flipped = true);
+        $('#hand').append(
+            returnPageDOM()
+        );
+        for (let i = 0; i < 3; i++) {
+            $('#hand').prepend(
+                returnCardDOM(tarotCards[i])
+            );
 
-        let hand = [];
-        const cardsToDraw = 3;
-        let page = 0;
-
-        // Draw three
-        for (let i = 0; i < cardsToDraw; i++) {
-            hand.push(tarotCards[i]);
+            $('.desc').prepend(
+                returnInfoDOM(tarotCards[i])
+            );
         }
 
-        // Render drawn cards
-        updateCards(hand);
+        let latestCard = 0;
 
-        $('#hand').on('mousedown', '.card', function() {
-            const index = $(this).index();
-            if (index === $('#hand .card').length-1) return;
-            hand[index].flipped = !hand[index].flipped;
-            $('#hand .card').eq(index).toggleClass('flipped');
-            $('#tabs .card').eq(index).toggleClass('flipped');
+        $('#look').on('click', '.card', function() {
+            // Clicked card flipped
+            $(this).toggleClass('flipped');
+            $('#hand .card').eq(latestCard).toggleClass('flipped');
         });
 
-        $('#tabs').on('mousedown', '.card', function() {
+        $('#hand').on('click', '.card', function() {
             const index = $(this).index();
-            if (page !== index) {
-                for (let i = 0; i < $('#hand .card').length; i++) {
-                    if (i === index) {
-                        $('#hand .card').eq(i).slideDown();
-                    } else {
-                        $('#hand .card').eq(i).slideUp();
-                    }
-                }
-                page = index;
-            }
+            if (latestCard === index) return;
+            latestCard = index;
+
+            // Remove duplicate cards
+            const search = $(this).find('.front img').attr('src');
+            $('#look .card').each(function() {
+                const src = $(this).find('.front img').attr('src');
+                if (search === src) $(this).remove();
+            });
+
+            // Append clicked card
+            $('#look').append(
+                $(this).clone()
+            );
+
+            // Clicked card selected
+            $('#hand .card').removeClass('selected');
+            $(this).addClass('selected');
         });
 
     });
